4d1f8edda05f12af39e0840180dd9a39ff4e0267
Mikhail <probel54@yandex.ru>
2021-12-08 09:33:50 +0700

    Initial Commit

---
+++ SwiftBookHelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftBookHelloWorld
+//
+//  Created by m.kushakov on 08.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
